51819f14b5ba95e448208d2e582648d01ac70b36
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-08-17 06:10:33 -0300

    feat: add consumer to main

---
+++ main/consumer.py
@@ -0,0 +1,22 @@
+import pika
+
+params = pika.URLParameters("amqps://bzosxusq:r0IheCfzOwXA_q16uSvtxxXQnrhApYtz@jackal.rmq.cloudamqp.com/bzosxusq")
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print('Received in main')
+    print(body)
+
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
+
+print('Started consuming')
+
+channel.start_consuming()
+channel.close()
