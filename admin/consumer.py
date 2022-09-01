548f63821bb5572eeb409326d37ab637a6b1bd48
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-09-01 10:02:28 -0300

    feat: add default os environ in pika URL parameters

--- admin/consumer.py
+++ admin/consumer.py
@@ -4,23 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-params = pika.URLParameters(os.environ.get('AMQP_URI'))
+params = pika.URLParameters(os.environ.get("AMQP_URI", ""))
 
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='admin')
+channel.queue_declare(queue="admin")
 
 
 def callback(ch, method, properties, body):
-    print('Received in admin')
+    print("Received in admin")
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue="admin", on_message_callback=callback, auto_ack=True)
 
-print('Started consuming')
+print("Started consuming")
 
 channel.start_consuming()
 channel.close()
