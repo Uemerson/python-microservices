e2c37cf9a27f448307ff7046798237be66cfdecd
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-09-01 09:58:08 -0300

    feat: add operations in database on consume queue

--- main/consumer.py
+++ main/consumer.py
@@ -1,26 +1,48 @@
+import json
 import pika
 import os
 from dotenv import load_dotenv
+from main import Product, db
 
 load_dotenv()
 
-params = pika.URLParameters(os.environ.get('AMQP_URI'))
+params = pika.URLParameters(os.environ.get("AMQP_URI", ""))
 
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='main')
+channel.queue_declare(queue="main")
 
 
 def callback(ch, method, properties, body):
-    print('Received in main')
-    print(body)
+    print("Received in main")
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == "product_created":
+        product = Product(id=data["id"], title=data["title"], image=data["image"])
+        db.session.add(product)
+        db.session.commit()
+        print("Product Created")
+
+    elif properties.content_type == "product_updated":
+        product = Product.query.get(data["id"])
+        product.title = data["title"]
+        product.image = data["image"]
+        db.session.commit()
+        print("Product Updated")
+
+    elif properties.content_type == "product_deleted":
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print("Product Deleted")
 
 
-channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue="main", on_message_callback=callback, auto_ack=True)
 
-print('Started consuming')
+print("Started consuming")
 
 channel.start_consuming()
 channel.close()
