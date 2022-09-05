096d4a720bd5270ef6a7f1642d90f0d516be5843
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-09-05 01:02:14 -0300

    feat: allowed all hosts in django app

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-h6al1p3&x4*%jg0h01*l*ttiofw*mg6q@*j*=s9el*yp5&cr%e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
