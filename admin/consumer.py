c181308c404090b3cf79aaae68129a6cf90173ae
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-09-05 01:28:20 -0300

    feat: consumer queue admin

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,6 +1,13 @@
 import pika
 import os
+import django
 from dotenv import load_dotenv
+import json
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
 
 load_dotenv()
 
@@ -15,7 +22,12 @@ channel.queue_declare(queue="admin")
 
 def callback(ch, method, properties, body):
     print("Received in admin")
-    print(body)
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print("Product likes increased!")
 
 
 channel.basic_consume(queue="admin", on_message_callback=callback, auto_ack=True)
